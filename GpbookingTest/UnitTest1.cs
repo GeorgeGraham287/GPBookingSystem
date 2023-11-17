fdb496cd3372397079039164f9c8984e0ab1d9fc
Alireza Khandan <AK714@canterbury.ac.uk>
2023-11-17 20:29:07 +0000

    added three tests in the UnitTest1 to check these issues: Test for a successful login, Test for an unsuccessful login (invalid email), Test for an unsuccessful login (invalid password)

--- GpbookingTest/UnitTest1.cs
+++ GpbookingTest/UnitTest1.cs
@@ -27,7 +27,7 @@ namespace GpbookingTest
 
             Assert.True(result);
         }
-
+        // test for invalid Email
         [Fact]
         public void ValidateEmail_InvalidEmail_ReturnsFalse()
         {
@@ -39,6 +39,7 @@ namespace GpbookingTest
 
             Assert.False(result);
         }
+        //test for valid Email
         [Fact]
         public void ValidateEmail_ValidEmail_ReturnsTrue()
         {
@@ -49,6 +50,58 @@ namespace GpbookingTest
 
             Assert.True(result);
         }
+
+
+
+        // Test for a successful login
+        [Fact]
+        public void AttemptLogin_ValidCredentials_ReturnsTrue()
+        {
+            // Arrange
+            BookingSystem bookingSystem = new BookingSystem();
+            Patient patient = new Patient("Alireza", "KHH", "AK714@canterbury.ac.uk", "passwordP123gg");
+            bookingSystem.RegisteredPatients.Add(patient);
+
+            // Act
+            bool result = bookingSystem.AttemptLogin("AK714@canterbury.ac.uk", "passwordP123gg");
+
+            // Assert
+            Assert.True(result);
+            Assert.Equal(patient, bookingSystem.LoggedInPatient);
+        }
+
+        // Test for an unsuccessful login (invalid email)
+        [Fact]
+        public void AttemptLogin_InvalidEmail_ReturnsFalse()
+        {
+            // Arrange
+            BookingSystem bookingSystem = new BookingSystem();
+
+            // Act
+            bool result = bookingSystem.AttemptLogin("invalidemail@example.com", "password");
+
+            // Assert
+            Assert.False(result);
+            Assert.Null(bookingSystem.LoggedInPatient);
+        }
+
+        // Test for an unsuccessful login (invalid password)
+        [Fact]
+        public void AttemptLogin_InvalidPassword_ReturnsFalse()
+        {
+            // Arrange
+            BookingSystem bookingSystem = new BookingSystem();
+            Patient patient = new Patient("Alex", "Dave", "jane@example.com", "password123");
+            bookingSystem.RegisteredPatients.Add(patient);
+
+            // Act
+            bool result = bookingSystem.AttemptLogin("jane@example.com", "invalidpassword");
+
+            // Assert
+            Assert.False(result);
+            Assert.Null(bookingSystem.LoggedInPatient);
+        }
+
     }
 
 }
